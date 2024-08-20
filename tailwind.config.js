b7a6cced39b8aff283412f08b6dbb58e07631e0c
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-08-20 22:54:44 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: "#1a202c", // ダークモードの背景色
+        darkCardBg: "#2d3748", // ダークモードのカード背景色
+        darkText: "#a0aec0", // ダークモードのテキスト色
+        darkAccent: "#4FD1C5", // ダークモードのアクセントカラー
+        darkBorder: "#4A5568", // ダークモードのボーダー色
+      },
+    },
   },
   plugins: [],
-}
-
+};
